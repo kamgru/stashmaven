de64442746723daa96d7b49f5d625ca92fb911d1
kamgru <kamil.grudzien@hotmail.com>
2024-02-09 16:02:30 +0100

    add partners list common stuff

--- src/stashmavenclient/src/app/partners/partners.component.ts
+++ src/stashmavenclient/src/app/partners/partners.component.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PartnerPreviewComponent} from "./partner-preview/partner-preview.component";
 import {RouterLink} from "@angular/router";
-import {SelectPartnerService} from "./select-partner.service";
 import {ListPartnersComponent} from "../common/list-partners/list-partners.component";
 
 @Component({
