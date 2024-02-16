bd6182704e822696c7ce7dfb90b44225f8d61408
kamgru <kamil.grudzien@hotmail.com>
2024-02-16 19:38:51 +0100

    small improvements

--- src/stashmavenclient/src/app/partners/partners.component.ts
+++ src/stashmavenclient/src/app/partners/partners.component.ts
@@ -3,13 +3,20 @@ import {CommonModule} from '@angular/common';
 import {PartnerPreviewComponent} from "./partner-preview/partner-preview.component";
 import {RouterLink} from "@angular/router";
 import {ListPartnersComponent} from "../common/list-partners/list-partners.component";
+import {CreatePartnerComponent} from "./create-partner/create-partner.component";
 
 @Component({
     selector: 'app-partners',
     standalone: true,
-    imports: [CommonModule, ListPartnersComponent, PartnerPreviewComponent, RouterLink],
+    imports: [CommonModule, ListPartnersComponent, PartnerPreviewComponent, RouterLink, CreatePartnerComponent],
     templateUrl: './partners.component.html',
     styleUrls: ['./partners.component.css']
 })
 export class PartnersComponent {
+
+    public uiState: 'list' | 'edit' = 'list';
+
+    handleAddPartner() {
+        this.uiState = 'edit';
+    }
 }
