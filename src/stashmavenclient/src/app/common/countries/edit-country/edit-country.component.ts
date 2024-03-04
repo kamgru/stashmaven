535b33a0d3c3ab1578cd1b6e396f9311e62a7be6
kamgru <kamil.grudzien@hotmail.com>
2024-03-04 21:29:36 +0100

    stuff

---
+++ src/stashmavenclient/src/app/common/countries/edit-country/edit-country.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-country',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-country.component.html',
+  styleUrl: './edit-country.component.css'
+})
+export class EditCountryComponent {
+
+}
